4c1b0f46ab298380dfd7e9f413eb16c901554fcc
testlyhealth <testlyhealth@gmail.com>
2025-06-16 16:03:05 +0100

    supabase auth codes update

--- js/api/supabase.js
+++ js/api/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const supabaseUrl = 'https://dicrvakkiopimvoumtat.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpY3J2YWtraW9waW12b3VtdGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNDkyMDIsImV4cCI6MjA2NDYyNTIwMn0.rbqgBdab9nJn_ilhuR5IMvoqMPZNbZcrETr6jpL9vGE'
+const supabaseUrl = 'https://zqgycplvddubvuuwkdrd.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxZ3ljcGx2ZGR1YnZ1dXdrZHJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4NDIyODksImV4cCI6MjA2NTQxODI4OX0.UKfMP5qtEt7aOjmTXbs8eUVuS5BYwda1k48EZIOsbyo'
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
