6fb920e03f66056e580fd4fca7f51bf20a888e34
testlyhealth <testlyhealth@gmail.com>
2025-07-18 20:02:25 +0100

    funnel updates

--- js/pages/compare.js
+++ js/pages/compare.js
@@ -10,7 +10,12 @@ export async function displayComparePage() {
     <section class="compare-content-section">
       <div class="compare-content">
         <div class="compare-grid">
-          <div class="compare-tile">
+          <div class="compare-tile option-specific">
+            <div class="tile-icon">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 21L16.514 16.506L21 21ZM19 10.5C19 15.194 15.194 19 10.5 19C5.806 19 2 15.194 2 10.5C2 5.806 5.806 2 10.5 2C15.194 2 19 5.806 19 10.5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </div>
             <div class="tile-content">
               <h3>1. I know what I want</h3>
               <p><em>"I already have a specific test or treatment in mind."</em></p>
@@ -22,7 +27,14 @@ export async function displayComparePage() {
             </div>
           </div>
 
-          <div class="compare-tile">
+          <div class="compare-tile option-concern">
+            <div class="tile-icon">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M12 8V12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M12 16H12.01" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </div>
             <div class="tile-content">
               <h3>2. I have a health concern</h3>
               <p><em>"I have symptoms or a goal but I'm not sure which test or treatment is right."</em></p>
@@ -34,7 +46,13 @@ export async function displayComparePage() {
             </div>
           </div>
 
-          <div class="compare-tile">
+          <div class="compare-tile option-checkup">
+            <div class="tile-icon">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9 12L11 14L15 10" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </div>
             <div class="tile-content">
               <h3>3. I want a general health check</h3>
               <p><em>"I'm looking for a complete health MOT or wellness package."</em></p>
@@ -46,7 +64,14 @@ export async function displayComparePage() {
             </div>
           </div>
 
-          <div class="compare-tile">
+          <div class="compare-tile option-browse">
+            <div class="tile-icon">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3 3H21V21H3V3Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M3 9H21" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M9 21V9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </div>
             <div class="tile-content">
               <h3>4. I'm just browsing</h3>
               <p><em>"I want to explore what's out there before deciding."</em></p>
